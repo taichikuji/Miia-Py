85747298cabef300a34d6056114670410f8b069d
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2021-05-10 06:10:46 +0000

    Move flake8 file to correct folder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from glob import iglob
 from os import sep
 
-from config import TOKEN
 from aiohttp import ClientSession
+from config import TOKEN
 from discord import Activity, ActivityType, Intents
 from discord.ext import commands
 
